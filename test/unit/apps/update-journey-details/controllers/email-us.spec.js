cd0dce0e2a559f1480f24bb76942edd676faa48e
Ralph Cowling <speak@ralph-cowling.com>
2016-06-03 17:25:15 +0100

    Test EmailUsController locals 📚

---
+++ test/unit/apps/update-journey-details/controllers/email-us.spec.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const controllers = require('hof').controllers;
+let Controller = controllers.base;
+let EmailUsController = require('../../../../../apps/update-journey-details/controllers/email-us');
+
+describe('apps/update-journey-details/controllers/email-us', function () {
+
+  describe('#locals', function () {
+
+    let controller;
+    let req;
+    let res;
+    let callback; // y u no eslint?
+
+    beforeEach(function () {
+      req = {
+        params: {},
+        form: {
+          values: {
+          }
+        },
+        sessionModel: {
+          get: sinon.stub(),
+          toJSON: sinon.stub().returns({
+            'transport-options': 'by-plane'
+          })
+        }
+      };
+      res = {};
+
+      controller = new EmailUsController({template: 'index'});
+    });
+
+    it('calls session model toJSON', function () {
+      controller.locals(req, res);
+
+      req.sessionModel.toJSON.should.have.been.calledOnce;
+    });
+
+    it('adds transport and list', function () {
+      let ret = controller.locals(req, res);
+      ret.should.have.property('transport').eql('by-plane');
+      ret.should.have.property('list').to.be.instanceof(Array);
+    });
+
+  });
+
+});
