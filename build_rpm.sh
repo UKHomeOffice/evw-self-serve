d6e12096016676f663c6dc2b47383994ffcc263f
Ralph <ralph@ralph-cowling.com>
2016-06-10 13:26:54 +0100

    Add in effing package management for building rpms (#1)
    * Add fpm build stuff.
    
    * Add in version string, set by travis.
    
    * Add in missing fail() function.

---
+++ build_rpm.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+function fail() {
+  echo "Failed: $@"
+  exit 1
+}
+
+
+# Read app version from root level version.properties
+updatesVersion=`cat version`
+
+TARGET=fpm/build/usr/share/evw-self-serve
+
+rm -rf ${TARGET}/*
+cp -rf app.js config.js CONTRIBUTING.md LICENSE package.json README.md version apps lib assets errors lib middleware $TARGET/
+
+cd fpm
+
+fpm --before-install scripts/preinstall.sh \
+    --after-install scripts/postinstall.sh \
+    --before-remove scripts/preuninstall.sh \
+    --after-remove scripts/postuninstall.sh \
+    -C build -t rpm -s dir -d nodejs \
+    -p NAME_VERSION.ARCH.TYPE \
+    -n evw-self-serve -v "$updatesVersion-$BUILD_NUMBER" -a all . \
+    || fail "Failed to build rpm"
+
+
