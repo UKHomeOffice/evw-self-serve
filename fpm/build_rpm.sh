f895f968dbb135fff06f1179210d60f2e4421427
Ralph Cowling <speak@ralph-cowling.com>
2016-06-14 11:44:14 +0100

    Attempt to get fpm builds into travis 📦

--- fpm/build_rpm.sh
+++ fpm/build_rpm.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-
 function fail() {
   echo "Failed: $@"
   exit 1
 }
 
-
-# Read app version from root level version.properties
+# Read app version version
 updatesVersion=`cat version`
 
 TARGET=fpm/build/usr/share/evw-self-serve
 
-rm -rf ${TARGET}/*
-cp -rf app.js config.js CONTRIBUTING.md LICENSE package.json README.md version apps lib assets errors lib middleware $TARGET/
+mkdir ${TARGET}
+cp -rf app.js config.js CONTRIBUTING.md LICENSE package.json README.md version apps lib assets errors lib middleware node_modules public $TARGET/
 
 cd fpm
+bundle install
 
 fpm --before-install scripts/preinstall.sh \
     --after-install scripts/postinstall.sh \
