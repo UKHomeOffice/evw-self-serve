44f639ec8b8596053b664348b68b5fd8e1126bf3
shepda <david.shepherd@transformuk.com>
2016-07-08 10:32:11 +0100

    Departure date validation

---
+++ validation/departure-date.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const moment = require('moment');
+
+module.exports = {
+  validate: (fieldValue, model) => {
+    let departureDate = moment(fieldValue, 'YYYY-M-D');
+    let arrivalDateMinusOneDay = moment(model.get('arrival-date')).subtract(1, 'day');
+    let arrivalDatePlusOneDay = moment(model.get('arrival-date')).add(1, 'day');
+
+    if (!departureDate.isValid()) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'departure-date.invalid'
+        }
+      };
+    }
+
+    if (departureDate.isBefore(arrivalDateMinusOneDay)) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'departure-date.in-past'
+        }
+      };
+    }
+
+    if (departureDate.isAfter(arrivalDatePlusOneDay)) {
+      return {
+        length: {
+          minimum: 12,
+          message: 'departure-date.in-future'
+        }
+      };
+    }
+
+    return false;
+  }
+};
