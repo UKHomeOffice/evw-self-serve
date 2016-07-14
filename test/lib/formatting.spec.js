eeca461c28f1eef37cda630a882cb92662746cde
Ralph Cowling <speak@ralph-cowling.com>
2016-07-14 13:54:02 +0100

    Add getDate to formatting
    - tests for padding out potential user values like '2-1-16' to '02-01-2016'

--- test/lib/formatting.spec.js
+++ test/lib/formatting.spec.js
@@ -2,8 +2,8 @@
 
 const formatting = require('../../lib/formatting');
 let values = {
-    'departure-date': '10-10-2016',
-    'departure-date-day': '10',
+    'departure-date': '',
+    'departure-date-day': '24',
     'departure-date-month': '10',
     'departure-date-year': '2016',
     'departure-time': '',
@@ -36,6 +36,21 @@ describe('lib/formatting', function () {
       values['departure-time-minutes'] = '';
       formatting.getTime(values, 'departure-time')
       .should.equal('18:00');
+    });
+  });
+
+  describe('#getDate', function () {
+    it('formats as DD-MM-YYYY', function () {
+      formatting.getDate(values, 'departure-date')
+      .should.equal('24-10-2016');
+    });
+
+    it('pads to DD-MM-YYYY', function () {
+      values['departure-date-day'] = '2'
+      values['departure-date-month'] = '3'
+      values['departure-date-year'] = '16'
+      formatting.getDate(values, 'departure-date')
+      .should.equal('02-03-2016');
     });
   });
 });
