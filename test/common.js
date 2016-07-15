6f776c1a8bdce888409221a432de32f44e64df34
Ralph Cowling <speak@ralph-cowling.com>
2016-07-15 15:11:11 +0100

    Spin up/kill dyson programmatically
    - Allows us to spin up and kill dyson stub serving within the context of the mocha runtime
    - This means running e.g. `mocha -w` won't break the second time your tests run due to port collision errors
    - It's not the most elegant thing but what is elegance anyway

--- test/common.js
+++ test/common.js
@@ -7,10 +7,27 @@ global.should = chai.should();
 global.sinon = require('sinon');
 require('sinomocha')();
 require('moment-business');
-
-// bring up evw-integration-stub
-require('evw-integration-stub')();
+require('colour');
 
 process.env.NODE_ENV = 'test';
 process.setMaxListeners(0);
 process.stdout.setMaxListeners(0);
+
+const path = require('path');
+const spawn = require('child_process').spawn;
+
+global.dysonServer = (options, doneCallback) => {
+  let name = options.name || 'Dyson';
+  let cmd = spawn('node', ['./node_modules/.bin/dyson', path.resolve(options.mocks), options.port]);
+
+  cmd.stdout.on('data', (data) => {
+    console.log(' ', data.toString().replace(/dyson/i, name).green);
+    doneCallback();
+  });
+
+  cmd.on('close', (code, signal) => {
+    console.log(`   ${name} terminated (${signal})`.yellow);
+  });
+
+  return cmd;
+}
