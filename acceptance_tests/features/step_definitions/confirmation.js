118e75b08fe2ff5ec6e028aecfdd8b6ec81d61b3
Ralph Cowling <speak@ralph-cowling.com>
2016-07-20 15:23:22 +0100

    Check for variable length evw reference id
    - The reference id comes back in various sizes but I *think* they're all above 7

--- acceptance_tests/features/step_definitions/confirmation.js
+++ acceptance_tests/features/step_definitions/confirmation.js
@@ -4,9 +4,9 @@ module.exports = function () {
 
     this.Then(/^the reference number should be present$/, function () {
         this.getText('.header-notice-complete #referenceId', function (ref) {
-          this.assert.equal(ref.value.length, 8);
+          let referenceThere = ref.value.length > 7;
+          this.assert.equal(referenceThere, true);
         });
     });
 
 };
-
