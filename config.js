e9b1151defaa700c69e73e833cc7c632a36e0d00
David Thompson <davethompson21@gmail.com>
2016-07-22 12:11:17 +0100

    Adding correct content to 404 page
    * fixes funky title
    * add default `appPath` to config

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ module.exports = {
   listen_host: process.env.LISTEN_HOST || '0.0.0.0',
   assetPath: process.env.ASSET_PATH || '/public',
   govukAssetPath: process.env.GOVUK_ASSET_PATH || '/govuk-assets',
+  appPath: 'find-your-application/enter-your-details',
   session: {
     secret: process.env.SESSION_SECRET || 'howdoesyourgardengrow',
     ttl: process.env.SESSION_TTL || 1200 /* 20 mins */
