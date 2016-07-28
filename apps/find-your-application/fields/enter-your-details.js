81b4effd76c38c23e24799876456e2d63a54d8ae
shepda <david.shepherd@transformuk.com>
2016-07-28 17:14:24 +0100

    Remove default validators as these are now covered by the custom date of birth validation rule

--- apps/find-your-application/fields/enter-your-details.js
+++ apps/find-your-application/fields/enter-your-details.js
@@ -1,25 +1,25 @@
 'use strict';
 
 module.exports = {
-    'evw-number': {
-        validate: ['required', 'alphanum']
-    },
-    'dob': {
-        validate: ['required', 'numeric', 'future'],
-        type: [ 'date' ],
-        legend: 'fields.date-of-birth.legend',
-        hint: 'fields.dob.hint'
-    },
-    'dob-day': {
-        validate: ['required', 'numeric'],
-        label: 'fields.dob-day.label'
-    },
-    'dob-month': {
-        validate: ['required', 'numeric'],
-        label: 'fields.dob-month.label'
-    },
-    'dob-year': {
-        validate: ['required', 'numeric', 'date-year'],
-        label: 'fields.dob-year.label'
-    }
+  'evw-number': {
+    validate: ['required', 'alphanum']
+  },
+  'dob': {
+    validate: ['future'],
+    type: [ 'date' ],
+    legend: 'fields.date-of-birth.legend',
+    hint: 'fields.dob.hint'
+  },
+  'dob-day': {
+    validate: ['required', 'numeric'],
+    label: 'fields.dob-day.label'
+  },
+  'dob-month': {
+    validate: ['required', 'numeric'],
+    label: 'fields.dob-month.label'
+  },
+  'dob-year': {
+    validate: ['required', 'numeric', 'date-year'],
+    label: 'fields.dob-year.label'
+  }
 };
