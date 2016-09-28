afa38deb79707979734608045c74b6d678074aea
Ralph Cowling <speak@ralph-cowling.com>
2016-09-28 18:52:43 +0100

    es6ify base controller
    - nothing new, just an es6 makeover
    - logger now logs errors, even when not in production (was annoying me that errors were being hidden by logger in development)

--- apps/common/controllers/evw-base.js
+++ apps/common/controllers/evw-base.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const util = require('util');
+
 const controllers = require('hof').controllers;
 const DateController = controllers.date;
 const ErrorClass = require('hof').controllers.error;
@@ -9,71 +9,70 @@ const logger = require('../../../lib/logger');
 const formatting = require('../../../lib/formatting');
 const validationRules = require('evw-validation-rules')['evw-self-serve'];
 
-let EvwBaseController = function EvwBaseController() {
-  DateController.apply(this, arguments);
-};
+module.exports = class EvwBaseController extends DateController {
 
-util.inherits(EvwBaseController, DateController);
-
-// A hack because Ralph can't figure out
-// passing options to a constructor 🙍🏽
-EvwBaseController.prototype.getNextStep = function (req, res) {
-  this.confirmStep = '/check-your-answers';
-  return DateController.prototype.getNextStep.call(this, req, res);
-}
-
-EvwBaseController.prototype.process = function process(req, res, callback) {
-  return DateController.prototype.process.call(this, req, res, function processTime() {
-    if(this.timeKey) {
-      req.form.values[this.timeKey] = formatting.getTime(req.form.values, this.timeKey);
-    }
-    callback();
-  }.bind(this));
-};
-
-EvwBaseController.prototype.applyDatesTimes = function firstDates(fields) {
-  Object.keys(fields).forEach((key) => {
-    let type = fields[key].type;
-    if(type && type.indexOf('date') > -1) {
-      this.dateKey = key;
-    };
-    if(type && type.indexOf('time') > -1) {
-      this.timeKey = key;
-    };
-  });
-}
-
-EvwBaseController.prototype.validateField = function validateField(key, req) {
-
-  if(validationRules.hasOwnProperty(key)) {
-
-    let value = formatting.setDateTimes(req.form.values, key);
-    let rules = validationRules[key](value, req.sessionModel);
-
-    req.sessionModel.set(key, value);
-
-    let field = {};
-    field[key] = value;
-
-    let schema = {};
-    schema[key] = rules;
-
-    let validationErrors = validateLib.validate(field, schema, {
-      fullMessages: false,
-    });
-
-    // found custom rules, got an error
-    if (validationErrors !== undefined) {
-      return new ErrorClass(key, {
-        type: validationErrors[key]
-      });
-    }
-  } else {
-    logger.info(`No custom validation rules found for ${key}`);
+  constructor(options) {
+    super(options);
+    this.applyDatesTimes(options.fields);
   }
 
-  return DateController.prototype.validateField.apply(this, arguments);
+  getNextStep (req, res) {
+    this.confirmStep = '/check-your-answers';
+    return super.getNextStep(req, res);
+  }
 
-};
+  process(req, res, callback) {
+    return super.process(req, res, function processTime() {
+      if(this.timeKey) {
+        req.form.values[this.timeKey] = formatting.getTime(req.form.values, this.timeKey);
+      }
+      callback();
+    }.bind(this));
+  }
 
-module.exports = EvwBaseController;
+  applyDatesTimes(fields) {
+    Object.keys(fields).forEach((key) => {
+      let type = fields[key].type;
+      if(type && type.indexOf('date') > -1) {
+        this.dateKey = key;
+      };
+      if(type && type.indexOf('time') > -1) {
+        this.timeKey = key;
+      };
+    });
+  }
+
+  validateField(key, req) {
+
+    if(validationRules.hasOwnProperty(key)) {
+
+      let value = formatting.setDateTimes(req.form.values, key);
+      let rules = validationRules[key](value, req.sessionModel);
+
+      req.sessionModel.set(key, value);
+
+      let field = {};
+      field[key] = value;
+
+      let schema = {};
+      schema[key] = rules;
+
+      let validationErrors = validateLib.validate(field, schema, {
+        fullMessages: false,
+      });
+
+      // found custom rules, got an error
+      if (validationErrors !== undefined) {
+        return new ErrorClass(key, {
+          type: validationErrors[key]
+        });
+      }
+    } else {
+      logger.info(`No custom validation rules found for ${key}`);
+    }
+
+    return super.validateField(key, req);
+
+  };
+}
+
