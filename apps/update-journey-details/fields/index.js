00293ec5be6531e3a0812765ee82f2d795484b97
David Thompson <davethompson21@gmail.com>
2017-01-09 15:57:30 +0000

    Removing arrival date page
    * includes controller, view, fields and translations

--- apps/update-journey-details/fields/index.js
+++ apps/update-journey-details/fields/index.js
@@ -4,7 +4,6 @@ module.exports = Object.assign(
   require('./how-will-you-arrive'),
   require('./email-us'),
   require('./flight-number'),
-  require('./arrival-date'),
   require('./choose-departure-airport'),
   require('./is-this-your-flight'),
   require('./departure-date-and-time'),
