d3c9478b1d0398b67d0ec813e7936f08fdecabcf
David Thompson <davethompson21@gmail.com>
2017-01-11 16:30:47 +0000

    Adding unit tests for `departure-date` controller
    * adding `sinon-as-promised`
    * refactor flight lookup into it's own function to improve testability

--- apps/update-journey-details/controllers/departure-date.js
+++ apps/update-journey-details/controllers/departure-date.js
@@ -12,39 +12,43 @@ module.exports = class DepartureDateController extends EvwBaseController {
   }
 
   process(req, res, callback) {
-    super.process(req, res, () => {
-      let lookupData = flightLookup.formatPost({
-        flightNumber: req.sessionModel.get('flight-number'),
-        departureDateDay: req.form.values['departure-date-day'],
-        departureDateMonth: req.form.values['departure-date-month'],
-        departureDateYear: req.form.values['departure-date-year']
-      });
+    super.process(req, res, this.lookup.bind(this, req, res, callback));
+  }
 
-      logger.info('looking up flight', lookupData);
-
-      flightLookup.findFlight(lookupData.number, lookupData.date)
-        .then(foundData => {
-          logger.info('flight service response for', lookupData, foundData.body);
-          const flight = foundData.body.flights[0];
-
-          // Flight found
-          if (typeof flight !== 'undefined') {
-            const mappedFlight = flightLookup.mapFlight(flight, req.sessionModel);
-            req.sessionModel.set('flightDetails', mappedFlight);
-          }  else {
-            req.sessionModel.set('flightDetails', null);
-          }
-
-          // Flight not found, page gets set in steps.js
-          callback();
-        })
-        .catch(error => {
-          // timeout/error etc
-          logger.error('flight lookup error', error);
-          req.sessionModel.set('flightDetails', null);
-          callback();
-        });
+  lookup(req, res, callback) {
+    let lookupData = flightLookup.formatPost({
+      flightNumber: req.sessionModel.get('flight-number'),
+      departureDateDay: req.form.values['departure-date-day'],
+      departureDateMonth: req.form.values['departure-date-month'],
+      departureDateYear: req.form.values['departure-date-year']
     });
+
+    logger.info('looking up flight', lookupData);
+
+    return flightLookup.findFlight(lookupData.number, lookupData.date)
+      .then(foundData => {
+        //console.log(foundData);
+        logger.info('flight service response for', lookupData, foundData.body);
+        const flight = foundData.body.flights[0];
+        //console.log(flight);
+
+        // Flight found
+        if (typeof flight !== 'undefined') {
+          const mappedFlight = flightLookup.mapFlight(flight, req.sessionModel);
+          req.sessionModel.set('flightDetails', mappedFlight);
+        }  else {
+          req.sessionModel.set('flightDetails', null);
+        }
+
+        // Flight not found, page gets set in steps.js
+        callback();
+      })
+      .catch(error => {
+        // timeout/error etc
+        logger.error('flight lookup error', error);
+        req.sessionModel.set('flightDetails', null);
+        callback();
+      });
   }
 
   locals(req, res) {
