48cb8cfbfdf2f88a82a7e44263f77a008b544702
David Thompson <davethompson21@gmail.com>
2017-01-20 10:38:13 +0000

    Modifying `find-your-application` app to use new validation
    * store fields on request
    * set required dateKeys in steps
    * set correct validations on fields and update translations
    * custom `validateField` function to handle CASE_NOT_FOUND or INVALID_TOKEN response when trying to verify EVW number and DOB

--- apps/find-your-application/controllers/enter-your-details.js
+++ apps/find-your-application/controllers/enter-your-details.js
@@ -1,16 +1,12 @@
 'use strict';
 
 const EvwBaseController = require('../../common/controllers/evw-base');
+const controllers = require('hof').controllers;
+const ErrorClass = controllers.error;
 const lookup = require('../../../lib/evw-lookup');
 const logger = require('../../../lib/logger');
 
 module.exports = class EnterYourDetailsController extends EvwBaseController {
-
-  constructor(options) {
-    super(options);
-    super.applyDatesTimes(options.fields);
-  }
-
   process(req, res, callback) {
     super.process(req, res, () => {
       // reset any previous lookup errors
@@ -33,4 +29,23 @@ module.exports = class EnterYourDetailsController extends EvwBaseController {
       });
     });
   }
-}
+
+  validateField(key, req) {
+    if (key === 'evw-number') {
+      const defaultValidationErrors = super.validateField(key, req);
+      if (defaultValidationErrors) {
+        return defaultValidationErrors;
+      }
+      const err = req.sessionModel.get('evwLookupError');
+      if (err === 'CASE_NOT_FOUND' || err === 'INVALID_TOKEN') {
+        return new ErrorClass(key, {
+          key: key,
+          type: 'not-found',
+          redirect: undefined
+        });
+      }
+      return false;
+    }
+    return super.validateField(key, req);
+  }
+};
--- apps/find-your-application/fields/enter-your-details.js
+++ apps/find-your-application/fields/enter-your-details.js
@@ -5,21 +5,17 @@ module.exports = {
     validate: ['required', 'alphanum']
   },
   'dob': {
-    validate: ['required', 'numeric', 'future'],
-    type: ['date'],
+    validate: ['required', 'date', 'before'],
     legend: 'fields.date-of-birth.legend',
     hint: 'fields.dob.hint'
   },
   'dob-day': {
-    validate: ['required', 'numeric'],
     label: 'fields.dob-day.label'
   },
   'dob-month': {
-    validate: ['required', 'numeric'],
     label: 'fields.dob-month.label'
   },
   'dob-year': {
-    validate: ['required', 'numeric'],
     label: 'fields.dob-year.label'
   }
 };
