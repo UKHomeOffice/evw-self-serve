8a0e15fbf12077f17a444710a3a3469bfacf07a0
David Thompson <davethompson21@gmail.com>
2017-01-23 17:40:30 +0000

    Show UK departure port typeahead value on check your answers page

--- apps/update-journey-details/controllers/check-your-answers.js
+++ apps/update-journey-details/controllers/check-your-answers.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const EvwBaseController = require('../../common/controllers/evw-base');
+const getTypeaheadValue = require('../../../lib/typeahead-options').getTypeaheadValue;
+const allAirportsPortsStations = require('../../../lib/typeahead-options').all;
 
 module.exports = class CheckYourAnswersController extends EvwBaseController {
   locals(req, res) {
+    let displayProps = {};
+    if (req.sessionModel.get('know-departure-details') === 'Yes') {
+      const portToDisplay = getTypeaheadValue(req.sessionModel.get('uk-port-of-departure'), allAirportsPortsStations);
+      displayProps.ukPortOfDepartureDisplay = portToDisplay;
+    }
     return Object.assign({
       knowDepartureDetailsYes: req.sessionModel.get('know-departure-details') === 'Yes',
       knowDepartureDetailsNo: req.sessionModel.get('know-departure-details') === 'No'
-    }, super.locals.call(this, req, res));
+    }, displayProps, super.locals.call(this, req, res));
   }
 };
