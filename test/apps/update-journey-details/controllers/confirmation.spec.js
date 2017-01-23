ea125bacce779e36a16fe0791c8fade216766af9
David Thompson <davethompson21@gmail.com>
2017-01-23 11:59:57 +0000

    Amend sending of data to IS to include return travel details
    * date formats updated as well

--- test/apps/update-journey-details/controllers/confirmation.spec.js
+++ test/apps/update-journey-details/controllers/confirmation.spec.js
@@ -1,15 +1,20 @@
 'use strict';
 
 const moment = require('moment');
-let ConirmationController = require('../../../../apps/update-journey-details/controllers/confirmation');
-let propMap = ConirmationController.propMap;
+let ConfirmationController = require('../../../../apps/update-journey-details/controllers/confirmation');
+let propMap = ConfirmationController.propMap;
 let modelFixture = require('../../../fixtures/update-model');
 
 describe('apps/update-journey-details/controllers/confirmation', function () {
+  let model;
+
+  beforeEach(function() {
+    model = Object.assign({}, modelFixture);
+  });
 
   describe('#propMap', function () {
     it('returns mapped update ready for submission', function () {
-      propMap(modelFixture).should.deep.equal({
+      propMap(model).should.deep.equal({
         'membershipNumber' : 'ABC1234',
         'token' : 'token123',
         'arrivalTravel' : 'EK009',
@@ -23,7 +28,56 @@ describe('apps/update-journey-details/controllers/confirmation', function () {
         'portOfArrivalCode' : 'LGW',
         'inwardDepartureCountry': 'ARE',
         'inwardDeparturePort': 'Dubai',
-        'inwardDeparturePortCode': 'DXB'
+        'inwardDeparturePortCode': 'DXB',
+        'haveDepartureFromUkDetailsChanged': 'Yes',
+        'knowDepartureDetails': 'Yes',
+        'departureDate': '2017-01-30',
+        'departureTravel': 'FL1001',
+        'portOfDeparture': 'LGW'
+      });
+    });
+
+    it('returns mapped update when return journey is changed but is unknown', function() {
+      model['know-departure-details'] = 'No';
+      propMap(model).should.deep.equal({
+        'membershipNumber' : 'ABC1234',
+        'token' : 'token123',
+        'arrivalTravel' : 'EK009',
+        'arrivalDate' : '2016-10-10',
+        'arrivalTime' : '19:45',
+        'departureForUKDate' : '2016-10-10',
+        'departureForUKTime' : '01:01',
+        'flightDetailsCheck': 'Yes',
+        'dateCreated': moment().format('YYYY-MM-DD hh:mm:ss'),
+        'portOfArrival' : 'London - Gatwick',
+        'portOfArrivalCode' : 'LGW',
+        'inwardDepartureCountry': 'ARE',
+        'inwardDeparturePort': 'Dubai',
+        'inwardDeparturePortCode': 'DXB',
+        'haveDepartureFromUkDetailsChanged': 'Yes',
+        'knowDepartureDetails': 'No',
+        'ukDuration': '1 to 3 months'
+      });
+    });
+
+    it('returns mapped update when return journey not changed', function() {
+      model['travel-details-changed'] = 'No';
+      propMap(model).should.deep.equal({
+        'membershipNumber' : 'ABC1234',
+        'token' : 'token123',
+        'arrivalTravel' : 'EK009',
+        'arrivalDate' : '2016-10-10',
+        'arrivalTime' : '19:45',
+        'departureForUKDate' : '2016-10-10',
+        'departureForUKTime' : '01:01',
+        'flightDetailsCheck': 'Yes',
+        'dateCreated': moment().format('YYYY-MM-DD hh:mm:ss'),
+        'portOfArrival' : 'London - Gatwick',
+        'portOfArrivalCode' : 'LGW',
+        'inwardDepartureCountry': 'ARE',
+        'inwardDeparturePort': 'Dubai',
+        'inwardDeparturePortCode': 'DXB',
+        'haveDepartureFromUkDetailsChanged': 'No'
       });
     });
   });
