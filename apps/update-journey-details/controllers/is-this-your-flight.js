3d612eac5b906bc679c8dabfd3e01841bd3f10bb
David Thompson <davethompson21@gmail.com>
2017-02-07 16:13:16 +0000

    Converting `is-this-your-flight` controller to ES6 class

--- apps/update-journey-details/controllers/is-this-your-flight.js
+++ apps/update-journey-details/controllers/is-this-your-flight.js
@@ -1,18 +1,13 @@
 'use strict';
 
-const util = require('util');
 const BaseController = require('hof').controllers.base;
 
-const IsThisYourFlightController = function IsThisYourFlightController() {
-  BaseController.apply(this, arguments);
-};
-
-util.inherits(IsThisYourFlightController, BaseController);
-
-IsThisYourFlightController.prototype.locals = function locals(req, res) {
-  return Object.assign({
-    flightDetails: req.sessionModel.get('flightDetails')
-  }, BaseController.prototype.locals.call(this, req, res));
-};
+class IsThisYourFlightController extends BaseController {
+  locals(req, res) {
+    return Object.assign({
+      flightDetails: req.sessionModel.get('flightDetails')
+    }, super.locals(req, res));
+  }
+}
 
 module.exports = IsThisYourFlightController;
