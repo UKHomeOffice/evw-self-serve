10f095d731ecd5d5ed0696fded87c033ba9f9c61
David Thompson <davethompson21@gmail.com>
2017-02-07 16:34:32 +0000

    Redirect to flight-number step if flight details are not found
    * this will prevent the user from seeing a blank flight details table
    
    Also:
    * IsThisYourFlightController now extends from EvwBaseController

--- apps/update-journey-details/controllers/is-this-your-flight.js
+++ apps/update-journey-details/controllers/is-this-your-flight.js
@@ -1,8 +1,15 @@
 'use strict';
 
-const BaseController = require('hof').controllers.base;
+const EvwBaseController = require('../../common/controllers/evw-base');
 
-class IsThisYourFlightController extends BaseController {
+class IsThisYourFlightController extends EvwBaseController {
+  getValues(req, res, callback) {
+    if (!req.sessionModel.get('flightDetails')) {
+      return res.redirect('flight-number');
+    }
+    super.getValues(req, res, callback);
+  }
+
   locals(req, res) {
     return Object.assign({
       flightDetails: req.sessionModel.get('flightDetails')
