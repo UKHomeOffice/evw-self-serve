e272f495344b971da0cda2bd3f7ee766135cd402
Ralph Cowling <speak@ralph-cowling.com>
2017-03-22 17:06:17 +0000

    Switch flight lookup to use evw-ffs
    - removes config
    - removes `lib/flight-lookup` and subsequent tests
    - removes use of sessionModel in flightLookup methods
    - adds `setFlight` method, as with evw-hof

--- config.js
+++ config.js
@@ -23,14 +23,6 @@ module.exports = {
   mongo: {
     connectionString: process.env.MONGO_CONNECTION_STRING || 'mongodb://localhost:27017/evw-self-serve'
   },
-  flightService: {
-    url: process.env.FLIGHT_SERVICE_URL || 'http://localhost:9350',
-    timeout: 5000,
-    check: {
-        method: 'GET',
-        endpoint: 'check-flight'
-    }
-  },
   integrationService: {
     url: process.env.INTEGRATION_SERVICE_URL || 'http://localhost:9300',
     port: process.env.INTEGRATION_SERVICE_PORT || 9300,
