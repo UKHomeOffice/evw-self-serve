a61530eac9329bab2d78b995b30c0d7faadc1f3f
Jackson Hyde <jacksonjhyde@gmail.com>
2022-05-20 16:58:35 +0100

    UTC date validation (#153)

--- apps/update-journey-details/controllers/train-date-of-departure.js
+++ apps/update-journey-details/controllers/train-date-of-departure.js
@@ -3,9 +3,17 @@
 const EvwBaseController = require('../../common/controllers/evw-base');
 const ErrorClass = require('hof').controllers.error;
 const moment = require('moment');
+require('moment-timezone');
 const validators = require('../../../lib/validators');
+const stations = require('../../../data/stations.json');
 
 module.exports = class TrainDateOfDepartureController extends EvwBaseController {
+
+  getTrainStation(stationCodeName) {
+    const stationName = stationCodeName.substring(stationCodeName.indexOf('_') + 1);
+    return stations.find(s => s.name === stationName);
+  }
+
   validateField(key, req) {
     const defaultValidationErrors = super.validateField(key, req);
     if (defaultValidationErrors) {
@@ -17,10 +25,12 @@ module.exports = class TrainDateOfDepartureController extends EvwBaseController 
       const arrivalTime = req.form.values['train-arrival-time'];
       const departureDate = req.form.values['train-departure-date'];
       const departureTime = req.form.values['train-departure-time'];
-      const arrivalDateTime = moment(arrivalDate + ' ' + arrivalTime, 'YYYY-MM-DD HH:mm');
-      const departureDateTime = moment(departureDate + ' ' + departureTime, 'YYYY-MM-DD HH:mm');
+      const departureStation = this.getTrainStation(req.form.values['train-departure-station']);
 
-      const errorType = validators.validateDepartureDate(arrivalDateTime, departureDateTime, true);
+      const arrivalDateTime = moment(arrivalDate + ' ' + arrivalTime, 'YYYY-MM-DD HH:mm');
+      const departureDateTime = moment(departureDate + ' ' + departureTime, 'YYYY-MM-DD HH:mm').tz(departureStation.timezone, true).utc();
+
+      const errorType = validators.validateDepartureDate(arrivalDateTime, departureDateTime, false);
       if (errorType) {
         return new ErrorClass(key, {
           key: key,
